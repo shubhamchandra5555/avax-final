93d8677d5a61d749fc04584e9c09520680aacc33
shubhamchandra5555 <129369914+shubhamchandra5555@users.noreply.github.com>
2023-09-15 17:41:41 +0530

    Update module4.sol

--- module4.sol
+++ module4.sol
@@ -5,19 +5,40 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
+    // Store item data (item ID => item price)
+    mapping(uint256 => uint256) public itemPrices;
+
     constructor() ERC20("Degen Gaming Token", "DGT") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function redeem(uint256 amount) public {
-        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
-        _burn(msg.sender, amount);
-        // Implement the logic for redeeming items from the in-game store here.
+    function redeem(uint256 itemId) public {
+        require(balanceOf(msg.sender) >= itemPrices[itemId], "Insufficient balance");
+        
+        // Determine the logic to deliver the in-game item based on the itemId.
+        // This could involve updating the player's in-game inventory, database, or any other relevant actions.
+        // For this example, let's assume we update a player's inventory.
+        updatePlayerInventory(msg.sender, itemId);
+        
+        // Deduct the token amount based on the item's price.
+        _burn(msg.sender, itemPrices[itemId]);
     }
 
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
+    }
+
+    // Owner can set the price of an in-game item
+    function setItemPrice(uint256 itemId, uint256 price) public onlyOwner {
+        itemPrices[itemId] = price;
+    }
+
+    // Internal function to update a player's inventory (customize this for your game)
+    function updatePlayerInventory(address player, uint256 itemId) internal {
+        // Implement the logic to update the player's in-game inventory.
+        // For example, you might add the item to an array or mapping.
+        // Emit an event to log the item redemption for tracking purposes.
     }
 }
